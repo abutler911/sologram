43777fd9ffe0b2f4c6c99396b48a74ae7636453b
abutler911 <abutler911@gmail.com>
2025-03-19 17:43:35 -0600

    Fix: Cleaning up a couple of MongoDB errors

--- server/server.js
+++ server/server.js
@@ -44,10 +44,7 @@ async function startServer() {
       throw new Error("MongoDB connection string is not defined");
     }
 
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoURI);
     console.log("MongoDB connected successfully");
 
     try {
