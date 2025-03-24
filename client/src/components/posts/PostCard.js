5372ecc5af8092eae24493561c90b6feaa31680d
abutler911 <abutler911@gmail.com>
2025-03-23 22:22:58 -0600

    reformat the post card

--- client/src/components/posts/PostCard.js
+++ client/src/components/posts/PostCard.js
@@ -359,6 +359,13 @@ const Card = styled.article`
   max-width: 614px;
   margin: 0 auto;
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+
+  @media (max-width: 768px) {
+    max-width: 100%;
+    border-radius: 0;
+    border-left: none;
+    border-right: none;
+  }
 `;
 
 const CardHeader = styled.header`
