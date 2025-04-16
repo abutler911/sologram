4526cc002e8e239c8eb34d304ae638c9597b34fe
abutler911 <abutler911@gmail.com>
2025-04-16 09:18:53 -0600

    Fix: Implementing the register page

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,21 @@
+// routes/auth.js
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../config/cloudinary");
 const {
   login,
+  register,
   getMe,
   updateProfile,
   updateBio,
   promoteToCreator,
 } = require("../controllers/auth");
+
 const { protect, authorize } = require("../middleware/auth");
 
 router.post("/login", login);
+router.post("/register", register);
+
 router.get("/me", protect, getMe);
 router.put(
   "/update-profile",
